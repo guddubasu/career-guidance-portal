89f3f26882d692cd103e975fe1bb86ecd30362b0
PammyBanerjee9192 <banerjeepammy0106@gmail.com>
2025-09-22 16:16:42 +0530

    Profile section completed

--- root/frontend/src/pages/Profile.jsx
+++ root/frontend/src/pages/Profile.jsx
@@ -9,19 +9,19 @@ export default function Profile(){
         <div className="card-body text-start">
        <p className="col-12 fs-1" style={{color:"#0c3973ff"}}>Pammy Banerjee</p>
       <p className="fs-4 col-3" style={{color:"#004aad"}}>Email</p>
-      <p className="border border-3 border-primary px-1 py-2 mb-2 border-opacity-25 rounded-4 col-10 fs-5 text-start">Email</p>
+      <p className="border border-3 border-primary px-3 py-2 mb-2 border-opacity-25 rounded-4 col-10 fs-5 text-start">Email</p>
 
       <p className="fs-4 col-3" style={{color:"#004aad"}}>Username</p>
-      <p className="border border-3 border-primary px-1 py-2 mb-2 border-opacity-25 rounded-4 col-10 fs-5 text-start">Username</p>
+      <p className="border border-3 border-primary px-3 py-2 mb-2 border-opacity-25 rounded-4 col-10 fs-5 text-start">Username</p>
         
         <p className="fs-4 col-4" style={{color:"#004aad"}}>Phone Number</p>
-      <p className="border border-3 border-primary px-1 py-2 mb-2 border-opacity-25 rounded-4 col-10 fs-5 text-start">number</p>
+      <p className="border border-3 border-primary px-3 py-2 mb-2 border-opacity-25 rounded-4 col-10 fs-5 text-start">number</p>
 
       <p className="fs-4 col-3" style={{color:"#004aad"}}>Gender</p>
-      <p className="border border-3 border-primary px-1 py-2 mb-2 border-opacity-25 rounded-4 col-10 fs-5 text-start">Gender</p>
+      <p className="border border-3 border-primary px-3 py-2 mb-2 border-opacity-25 rounded-4 col-10 fs-5 text-start">Gender</p>
 
       <p className="fs-4 col-3" style={{color:"#004aad"}}>Location</p>
-      <p className="border border-3 border-primary px-1 py-2 mb-2 border-opacity-25 rounded-4 col-10 fs-5 text-start">location</p>
+      <p className="border border-3 border-primary px-3 py-2 mb-2 border-opacity-25 rounded-4 col-10 fs-5 text-start">location</p>
       <Link to="/profildetails"><button className="btn btn-primary fs-5 mt-4">Complete Profile</button></Link>
       <h2></h2>
     
